beba782ebe4338162a5ad54be76701fe5521cbec
EC2 Default User <ec2-user@ip-172-31-38-13.ap-northeast-1.compute.internal>
2019-03-06 08:06:10 +0000

    0303v1

---
+++ install_docker/installDocker_EC2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+echo 'please login again'
+exit
--- ngrok_url.sh
+++ ngrok_url.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 curl localhost:4040/api/tunnels > tunnels.json
 cat ./tunnels.json | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
